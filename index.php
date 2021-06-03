fbd6d2dd6835a248fa67f78ed82abe6d59f0f154
Bob Storms <bob.storms@hotmail.be>
2021-06-03 22:44:35 +0200

    added basic styling: typography, colors and max screen width

--- index.php
+++ index.php
@@ -24,10 +24,12 @@
     <title>Foodflow | Home</title>
 </head>
 <body>
-    <h1>Home</h1>
-    <p><strong>Welcome to my website!</strong></p>
-    <p>First name: <?php echo $res["first_name"]; ?></p>
-    <p>Last name: <?php echo $res["last_name"]; ?></p>
-    <p>Email: <?php echo $_SESSION["user"]; ?></p>
+    <main>
+        <h1>Home</h1>
+        <p><strong>Welcome to my website!</strong></p>
+        <p>First name: <?php echo $res["first_name"]; ?></p>
+        <p>Last name: <?php echo $res["last_name"]; ?></p>
+        <p>Email: <?php echo $_SESSION["user"]; ?></p>
+    </main>
 </body>
 </html>
